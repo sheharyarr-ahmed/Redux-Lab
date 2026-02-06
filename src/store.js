5f20454ebec9631127c860022fce6dfcd45c46e9
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-02-06 17:27:33 +0500

    got to knowe more about the react redux function

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -7,12 +9,17 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.paylaod };
+      return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
-      return { ...state, balance: state.balance - action.paylaod };
+      return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: 0, balance: state.balance + action.paylaod };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -25,3 +32,17 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 100 });
+console.log(store.getState());
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "to buy a car" },
+});
+console.log(store.getState());
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
