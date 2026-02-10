74b61ed01f8ff868f528bae299056a4bf57b6031
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-02-11 02:18:08 +0500

    refactor(store): migrate to configureStore and add legacy store-v2

--- src/store-v2.js
+++ src/store-v2.js
@@ -1,12 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-const store = configureStore({
-  reducer: {
-    account: accountReducer,
-    customer: customerReducer,
-  },
+import { composeWithDevTools } from "redux-devtools-extension";
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 export default store;
 
 // previous reducer function
